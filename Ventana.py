344f18881b0eb885f02a36078a206679f1f5fbe2
Miguel-Sanchez241001 <msanchez24@autonoma.edu.pe>
2020-12-18 01:47:42 -0500

    AGREGANDO DATA REAL

--- Ventana.py
+++ Ventana.py
@@ -4,7 +4,6 @@ class Ventana:
     p2 = ""
     p3 = ""
     p4 = ""
-
     def __init__(self,p1, p2,p3,p4):
         self.p1 = p1
         self.p2 = p2
@@ -13,19 +12,19 @@ class Ventana:
         raiz = Tk()
         raiz.title("Bootstrap")
         raiz.resizable(False, False)
-        raiz.geometry("350x200")
+        raiz.geometry("300x200")
         label0 = Label(text ="Intervalos de confianza por percentiles")
         label0.place(x=20,y = 20)
         label1 = Label(text=p1)
-        label1.place(x=40, y=40)
+        label1.place(x=40, y=50)
         label2 = Label(text=p2)
-        label2.place(x=80, y=40)
+        label2.place(x=100, y=50)
         label0 = Label(text="Intervalos de confianza por 95%")
-        label0.place(x=20, y=60)
+        label0.place(x=20, y=80)
         label1 = Label(text=p3)
-        label1.place(x=40, y=80)
+        label1.place(x=40, y=110)
         label1 = Label(text=p4)
-        label1.place(x=80, y=80)
+        label1.place(x=100, y=110)
         raiz.mainloop()
 
 
--- bootstrap.py
+++ bootstrap.py
@@ -3,16 +3,18 @@ import matplotlib.pyplot as plt
 import statistics
 from Ventana import Ventana
 
-DATA = pd.read_csv('muertos.csv')# importando data
-EDAD = DATA['EDAD']# seleccionamos la columna
+DATA = pd.read_csv('muertosactu.csv')# importando data
+EDAD = DATA['EDAD'].sample(100,replace= True)# seleccionamos la columna
 mediasMuestras = [ ] #vector
 #DesEstandar = statistics.stdev(EDAD) # Desviacion estandar de la muestra inicial
 for i in range(5000):
 
-   Muestra = EDAD.sample(89, replace =  True)# Muestras aleatorias con reemplazo
+   Muestra = EDAD.sample(100, replace =  True)# Muestras aleatorias con reemplazo
    media = statistics.mean(Muestra)# media de cada muestra
    mediasMuestras.append(media) # agregamos a la lista de medias Muestrales
-# hsitograma
+
+
+# histograma
 plt.hist(mediasMuestras, color = 'blue', edgecolor = 'black',bins = 18)
 # titulos para el histograma
 plt.title('Histograma Bootstrap')
@@ -21,16 +23,22 @@ plt.ylabel('Numero de Fallecidos')
 plt.show()
 # intervalo de confianza al 95%
 Media = statistics.mean(EDAD) # Media de la muestra Incial
-EstandarError = statistics.stdev(mediasMuestras) # error estandar de la muestra inicial
-# limites para la precisión si la estimación de la muestra coincide con el valor de toda la población
+EstandarError = statistics.stdev(mediasMuestras) # error estandar
+# limites para la precisión si la estimación de la muestra
 limInferior = Media-2*EstandarError
 limSuperior = Media+2*EstandarError
-# conversion de mediasMuestra a pandas para calcular los cuantiles
+# conversion de mediasMuestra a pandas
 test = pd.Series(mediasMuestras)
 # cuantiles para el intervalo por percentiles
 p1 = test.quantile(0.025)
 p2 = test.quantile(0.975)
 # visualizacion de loss datos en una ventana
-ventana =Ventana(str(p1.round(2)),str(p2.round(2)),round(limInferior,2),round(limSuperior,2))
+ventana =Ventana(str(p1.round(2)),str(p2.round(2)),
+round(limInferior,2),round(limSuperior,2))
+
 #grafica de densidad
-pd.DataFrame(mediasMuestras, columns=['Edades']).plot(kind='density',grid = True, title = "Grafica de densidad" ) # grafica de densidad
+pd.DataFrame(mediasMuestras, columns=['Edades']).plot(kind='density',
+grid = True, title = "Grafica de densidad" ) # grafica de densidad
+
+
+print(type(EDAD))
